751e9e9fa44c793d994419aaf5956a457da15a95
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2025-03-19 13:51:22 +0700

    add login google

--- FE/babel.config.js
+++ FE/babel.config.js
@@ -1,20 +1,10 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: [
-      [
-          'module:react-native-dotenv',
-          {
-              envName: 'APP_ENV',
-              moduleName: '@env',
-              path: '.env',
-             
-          },
-      ],
-  ],
-  };
+    api.cache(true);
+    return {
+        presets: [
+            ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+            'nativewind/babel',
+        ],
+        
+    };
 };
--- FE/lib/util.ts
+++ FE/lib/util.ts
@@ -3,15 +3,16 @@ import * as SecureStore from 'expo-secure-store';
 
 export async function fetcher<T> (input: string, init?: RequestInit) {
   try {
-  
+    console.log(process.env.BACKEND_URL, 'backend')
     const response = await fetch(`${process.env.BACKEND_URL}/${input}`, init)
-  
     const data = await response.json()
+    console.log({data})
 
   
     return data as T | IError
   
   } catch (error) {
+    console.log({error})
     return {
       statusCode: 500,
       message: 'Server Error'
